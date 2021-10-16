e139b6825e1c705b166aa2dfce23f8c428497fb8
HoshiKurama <52633255+HoshiKurama@users.noreply.github.com>
2021-10-15 17:15:18 -0700

    Update dependencies

--- Paper/build.gradle.kts
+++ Paper/build.gradle.kts
@@ -20,8 +20,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
     implementation("com.github.HoshiKurama:KyoriComponentDSL:1.1.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.5.2-native-mt")
-    implementation("net.kyori:adventure-extra-kotlin:4.9.0")
-    implementation("joda-time:joda-time:2.10.11")
+    implementation("net.kyori:adventure-extra-kotlin:4.9.2")
+    implementation("joda-time:joda-time:2.10.12")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
     implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:1.5.0")
     implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-core:1.5.0")
@@ -35,7 +35,6 @@ tasks {
         dependencies {
             include(dependency("com.github.HoshiKurama:KyoriComponentDSL:1.1.0"))
             include(dependency("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.3.0"))
-            //include(dependency("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:1.3.0"))
             include(project(":common"))
 
             relocate("com.github.hoshikurama.componentDSL", "com.github.hoshikurama.ticketmanager.componentDSL")
--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("plugin.serialization") version "1.5.20"
+    kotlin("plugin.serialization") version "1.5.31"
     kotlin("jvm")
     java
 }
@@ -16,11 +16,11 @@ dependencies {
     implementation("com.github.jasync-sql:jasync-mysql:2.0.2")
     implementation("com.github.seratch:kotliquery:1.6.0")
     implementation("com.github.HoshiKurama:KyoriComponentDSL:1.1.0")
-    implementation("net.kyori:adventure-api:4.9.0")
-    implementation("net.kyori:adventure-extra-kotlin:4.9.0")
-    implementation("net.kyori:adventure-text-serializer-legacy:4.9.0")
+    implementation("net.kyori:adventure-api:4.9.2")
+    implementation("net.kyori:adventure-extra-kotlin:4.9.2")
+    implementation("net.kyori:adventure-text-serializer-legacy:4.9.2")
     implementation("org.yaml:snakeyaml:1.29")
-    implementation("joda-time:joda-time:2.10.11")
+    implementation("joda-time:joda-time:2.10.12")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.3.0")
     implementation("dev.kord:kord-core:0.7.4")
 }
