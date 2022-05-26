613342699bdf6b7eb537eac9efd27dc7578e4ee3
HoshiKurama <52633255+HoshiKurama@users.noreply.github.com>
2022-05-25 18:23:17 -0700

    Update dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -11,17 +11,17 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.20")
-    implementation("mysql:mysql-connector-java:8.0.28")
+    implementation("mysql:mysql-connector-java:8.0.29")
     implementation("com.github.jasync-sql:jasync-mysql:2.0.6")
     implementation("com.github.seratch:kotliquery:1.7.0")
     implementation("net.kyori:adventure-api:4.10.1")
     implementation("net.kyori:adventure-extra-kotlin:4.10.1")
-    implementation("net.kyori:adventure-text-minimessage:4.10.0")
+    implementation("net.kyori:adventure-text-minimessage:4.10.1")
     implementation("org.yaml:snakeyaml:1.30")
     implementation("joda-time:joda-time:2.10.14")
     implementation("com.discord4j:discord4j-core:3.2.2")
     implementation("com.google.code.gson:gson:2.9.0")
-    implementation("com.h2database:h2:2.1.210")
+    implementation("com.h2database:h2:2.1.212")
     implementation("com.google.guava:guava:31.1-jre")
 
 }
