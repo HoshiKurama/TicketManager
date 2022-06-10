475cbdee62ca927e9ab2383b731a822099c408e5
HoshiKurama <52633255+HoshiKurama@users.noreply.github.com>
2022-06-10 14:52:42 -0700

    Version update

--- common/src/main/kotlin/com/github/hoshikurama/ticketmanager/VariousInformation.kt
+++ common/src/main/kotlin/com/github/hoshikurama/ticketmanager/VariousInformation.kt
@@ -3,7 +3,7 @@ package com.github.hoshikurama.ticketmanager
 import java.util.*
 
 const val metricsKey = 11033
-const val pluginVersion = "8.2.1"
+const val pluginVersion = "8.2.2"
 
 val randServerIdentifier: UUID = UUID.randomUUID() // For proxies
 
