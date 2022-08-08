8232c0b3af38f94b896144039db9fb1bc607929e
HoshiKurama <52633255+HoshiKurama@users.noreply.github.com>
2022-08-08 15:14:46 -0700

    Update to 8.3.1

--- common/src/main/kotlin/com/github/hoshikurama/ticketmanager/common/VariousInformation.kt
+++ common/src/main/kotlin/com/github/hoshikurama/ticketmanager/common/VariousInformation.kt
@@ -2,7 +2,7 @@ package com.github.hoshikurama.ticketmanager.common
 
 import java.util.*
 
-const val mainPluginVersion = "8.3.0"
+const val mainPluginVersion = "8.3.1"
 const val bridgePluginVersion = "1.2"
 
 const val bukkitMetricsKey = 11033
@@ -13,13 +13,13 @@ val randServerIdentifier: UUID = UUID.randomUUID() // For proxies
 
 /*
 Things to do when updating version:
-    - UPDATING MAIN PLUGIN:
+    - UPDATING SE:
         1. Update main plugin version above.
         2. Update each config file (config.yml)
         3. Update each plugin version (plugin.yml)
         4. Update root build.gradle.kts
 
-    - UPDATING BRIDGE:
+    - UPDATING PBE:
         1. Update bridge plugin version above.
         2. Update each config file (velocity-plugin.json)
         3. Update each plugin version ()
