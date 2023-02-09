5091cf125e1c96308327d98a02b93618c1bdcb8a
HoshiKurama <Rankin.3Reynolds@gmail.com>
2023-02-09 15:18:33 -0800

    Update plugin versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
 
 subprojects {
     group = "com.github.hoshikurama"
-    version = "8.5.0"
+    version = "9.0.0"
 
     tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
