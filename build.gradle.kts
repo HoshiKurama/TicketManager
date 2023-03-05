ad4e7fa9b3f167dc1d626fa5d853dfdcba8046d4
HoshiKurama <52633255+HoshiKurama@users.noreply.github.com>
2023-03-05 09:00:28 -0800

    Increment SE to 9.0.1 Increment PBE to 2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
 
 subprojects {
     group = "com.github.hoshikurama"
-    version = "9.0.0"
+    version = "9.0.1"
 
     tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
