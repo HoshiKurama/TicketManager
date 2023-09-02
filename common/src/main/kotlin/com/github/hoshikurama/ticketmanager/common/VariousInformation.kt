0ce02ad349ae6de8374b5472b56ab4e6d7467b72
HoshiKurama <52633255+HoshiKurama@users.noreply.github.com>
2023-09-02 15:39:33 -0700

    Downgrade to 10.0.0 (FIXED)

--- common/src/main/kotlin/com/github/hoshikurama/ticketmanager/common/VariousInformation.kt
+++ common/src/main/kotlin/com/github/hoshikurama/ticketmanager/common/VariousInformation.kt
@@ -2,7 +2,7 @@ package com.github.hoshikurama.ticketmanager.common
 
 import java.util.*
 
-const val mainPluginVersion = "10.0.1"
+const val mainPluginVersion = "10.0.0"
 const val bridgePluginVersion = "3.0"
 
 const val bukkitMetricsKey = 11033
