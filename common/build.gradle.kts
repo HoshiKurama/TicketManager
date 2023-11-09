7583f9b4eb54d65d34f38074f19f7e699510bfe8
HoshiKurama <52633255+HoshiKurama@users.noreply.github.com>
2023-11-09 16:55:28 -0700

    Dependency Updates

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm")
-    id("com.github.ben-manes.versions") version "0.47.0" // https://github.com/ben-manes/gradle-versions-plugin
+    id("com.github.ben-manes.versions") version "0.49.0" // https://github.com/ben-manes/gradle-versions-plugin
 }
 
 repositories {
@@ -8,5 +8,5 @@ repositories {
 }
 
 dependencies {
-    compileOnly("com.google.guava:guava:32.1.2-jre")
+    compileOnly("com.google.guava:guava:32.1.3-jre")
 }
