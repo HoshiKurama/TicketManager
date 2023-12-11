148ec9f755525d0b229efdee3e15ab9ff262b7d7
HoshiKurama <52633255+HoshiKurama@users.noreply.github.com>
2023-12-11 11:40:27 -0700

    Dependency updates

--- ProxyBridge/Velocity/build.gradle.kts
+++ ProxyBridge/Velocity/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("com.github.ben-manes.versions") version "0.49.0" // https://github.com/ben-manes/gradle-versions-plugin
+    id("com.github.ben-manes.versions") version "0.50.0" // https://github.com/ben-manes/gradle-versions-plugin
     kotlin("jvm")
     java
     application
@@ -25,7 +25,7 @@ dependencies {
 
     implementation(project(":common"))
     implementation(project(":ProxyBridge:commonPDE"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
     implementation("org.bstats:bstats-velocity:3.0.2")
     implementation("org.yaml:snakeyaml:2.2")
 }
