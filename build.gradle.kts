3d6489a43eb014162a87f4d366471e3169de6918
HoshiKurama <52633255+HoshiKurama@users.noreply.github.com>
2023-12-11 11:48:18 -0700

    Forgot to update one line for dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.20")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.21")
 }
 
 subprojects {
